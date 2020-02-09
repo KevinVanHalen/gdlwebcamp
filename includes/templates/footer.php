e122b0ab20a763fcefb11e55fc3e2050b0c82e33
KevinVanHalen <keviinvanhalen@gmail.com>
2020-02-08 18:21:56 -0600

    Etapa avanzada del proyecto. Hasta antes de agregar paypal y lazona de administracion

---
+++ includes/templates/footer.php
@@ -0,0 +1,103 @@
+        <footer class="site-footer">
+            <div class="contenedor clearfix">
+                <div class="footer-informacion">
+                    <h3>Sobre <span>gdlwebcamp</span></h3>
+                    <p>Class aptent taciti sociosqu ad litora torquent 
+                        per conubia nostra, per inceptos himenaeos. 
+                        Nullam porttitor velit et lacus semper, in blandit
+                        augue porta. Mauris sit amet risus volutpat nisi 
+                        placerat consequat.
+                    </p>
+                </div>
+                <div class="ultimos-tweets">
+                    <h3>Últimos <span>tweets</span></h3>
+                    <ul>
+                        <li>Sed in mi convallis, aliquet libero id, fringilla ipsum. Nulla in pellentesque risus. Vivamus vitae augue libero.</li>
+                        <li>Sed in mi convallis, aliquet libero id, fringilla ipsum. Nulla in pellentesque risus. Vivamus vitae augue libero.</li>
+                        <li>Sed in mi convallis, aliquet libero id, fringilla ipsum. Nulla in pellentesque risus. Vivamus vitae augue libero.</li>
+                    </ul>
+                </div>
+                <div class="menu">
+                    <h3>Redes <span>sociales</span></h3>
+                    <nav class="redes-sociales">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-pinterest"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </nav>
+                </div>
+            </div>
+
+            <p class="copyright">
+                Todos los Derechos Reservados GDLWEBCAMP 2016.
+            </p>
+
+            <!-- Begin Mailchimp Signup Form -->
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+                #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div style="display:none;">
+                <div id="mc_embed_signup">
+                <form action="https://gmail.us5.list-manage.com/subscribe/post?u=e3fccc04c9c42fbfcd360bc3f&amp;id=40a1b004ed" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                    <h2>Suscribete al Newsletter y no te pierdas nada de este evento</h2>
+                <div class="indicates-required"><span class="asterisk">*</span> es obligatorio</div>
+                <div class="mc-field-group">
+                    <label for="mce-EMAIL">Correo Electrónico  <span class="asterisk">*</span>
+                </label>
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                </div>
+                    <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e3fccc04c9c42fbfcd360bc3f_40a1b004ed" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" value="Suscribirse" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                    </div>
+                </form>
+                </div>
+                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                <!--End mc_embed_signup-->
+            </div>
+
+        </footer>
+
+        <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/jquery.animateNumber.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/jquery.lettering.js"></script>
+
+        
+
+        <!-- <script src="js/jquery.colorbox-min.js"></script> -->
+        <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"></script>
+        <script src="js/main.js"></script>
+
+        <?php
+            $archivo = basename($_SERVER['PHP_SELF']);
+            $pagina = str_replace(".php", "", $archivo);
+            if($pagina == 'invitados' || $pagina == 'index'){
+                echo '<script src="js/jquery.colorbox-min.js"></script>';
+            }else if($pagina == 'conferencia') {
+                echo '<script src="js/lightbox.js"></script>';
+            }
+        ?>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+          window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+          ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+        <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us5.list-manage.com","uuid":"e3fccc04c9c42fbfcd360bc3f","lid":"40a1b004ed","uniqueMethods":true}) })</script>
+    </body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,226 @@
+(function() {
+    "use strict";
+
+    var regalo = document.getElementById('regalo');
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        var map = L.map('mapa').setView([20.673391, -103.346535], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([20.673391, -103.346535]).addTo(map)
+            .bindPopup('GDLWebCamp 2018 <br> Boletos ya disponibles')
+            .openPopup()
+            .bindTooltip('Un Tooltip')
+            .openTooltip();
+
+        //Campos Datos usuario
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var email = document.getElementById('email');
+
+        //Campos pases
+        var pase_dia = document.getElementById('pase_dia');
+        var pase_dosdias = document.getElementById('pase_dosdias');
+        var pase_completo = document.getElementById('pase_completo');
+
+        //Botones y divs
+        var calcular = document.getElementById('calcular');
+        var errorDiv = document.getElementById('error');
+        var botonRegistro = document.getElementById('btnRegistro');
+        var lista_productos = document.getElementById('lista-productos');
+        var suma = document.getElementById('suma-total');
+
+        //Extras
+        var etiquetas = document.getElementById('etiquetas');
+        var camisas = document.getElementById('camisa_evento');
+
+        botonRegistro.disabled = true;
+
+        if(document.getElementById('calcular')) {
+
+            calcular.addEventListener('click', calcularMontos);
+
+            pase_dia.addEventListener('blur', mostrarDias);
+            pase_dosdias.addEventListener('blur', mostrarDias);
+            pase_completo.addEventListener('blur', mostrarDias);
+
+            nombre.addEventListener('blur', validarCampos);
+            apellido.addEventListener('blur', validarCampos);
+            email.addEventListener('blur', validarCampos);
+            email.addEventListener('blur', validarMail);
+
+            function validarCampos() {
+                if(this.value == ''){
+                    errorDiv.style.display = 'block';
+                    errorDiv.innerHTML = 'Este campo es obligatorio';
+                    this.style.border = '1px solid red';
+                    errorDiv.style.border = '1px solid red';
+                }else {
+                    errorDiv.style.display = 'none';
+                    this.style.border = '1px solid #cccccc';
+                }
+            }
+
+            function validarMail() {
+                if(this.value.indexOf("@") > -1) {
+                    errorDiv.style.display = 'none';
+                    this.style.border = '1px solid #cccccc';
+                }else {
+                    errorDiv.style.display = 'block';
+                    errorDiv.innerHTML = 'El email debe ser valido';
+                    this.style.border = '1px solid red';
+                    errorDiv.style.border = '1px solid red';
+                }
+            }
+
+            function calcularMontos(event) {
+                event.preventDefault();
+
+                if(regalo.value === '') {
+                    alert("Debes elegir un regalo");
+                    regalo.focus();
+                }else {
+                    var boletosDia = parseInt(pase_dia.value, 10) || 0;
+                    var boletos2Dias = parseInt(pase_dosdias.value, 10) || 0;
+                    var boletoCompleto = parseInt(pase_completo.value, 10) || 0;
+                    var cantCamisas = parseInt(camisas.value, 10) || 0;
+                    var cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+
+                    var totalPagar = (boletosDia * 30) + (boletos2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+                    
+                    var listadoProductos = [];
+                    
+                    if(boletosDia >= 1) {
+                        listadoProductos.push(boletosDia + ' Pases por día');
+                    }
+                    if(boletos2Dias >= 1){
+                        listadoProductos.push(boletos2Dias + ' Pases por 2 día');
+                    }
+                    if(boletoCompleto >= 1){
+                        listadoProductos.push(boletoCompleto + ' Pases completos');
+                    }
+                    if(cantCamisas >= 1){
+                        listadoProductos.push(cantCamisas + ' Camisas');
+                    }
+                    if(cantEtiquetas >= 1) {
+                        listadoProductos.push(cantEtiquetas + ' Etiquetas');
+                    }
+
+                    lista_productos.style.display = 'block';
+                    lista_productos.innerHTML = '';
+
+                    for(var i=0; i<listadoProductos.length; i++) {
+                        lista_productos.innerHTML += listadoProductos[i] + '<br/>'
+                    }
+
+                    suma.innerHTML = '$ ' + totalPagar.toFixed(2);
+
+                    botonRegistro.disabled = false;
+                    document.getElementById('total_pedido').value = totalPagar;
+                    //console.log(listadoProductos);
+                    //console.log(totalPagar);
+                }
+            }
+
+            function mostrarDias() {
+                var boletosDia = parseInt(pase_dia.value, 10) || 0;
+                var boletos2Dias = parseInt(pase_dosdias.value, 10) || 0;
+                var boletoCompleto = parseInt(pase_completo.value, 10) || 0;
+                    
+                var diasElegidos = [];
+
+                if(boletosDia > 0) {
+                    diasElegidos.push('viernes');
+                }
+                if(boletos2Dias > 0) {
+                    diasElegidos.push('viernes', 'sabado');
+                }
+                if(boletoCompleto > 0) {
+                    diasElegidos.push('viernes', 'sabado', 'domingo');
+                }
+
+                for(var i=0; i < diasElegidos.length; i++) {
+                    document.getElementById(diasElegidos[i]).style.display = 'block';
+                }
+            }
+        }
+
+    }); //DOM CONTENT LOADED
+
+})();
+
+$(function() {
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+
+    // Agregar clase a Menú
+    $('body.conferencia .navegacion-principal a:contains("Conferencia")').addClass('activo');
+    $('body.calendario .navegacion-principal a:contains("Calendario")').addClass('activo');
+    $('body.invitados .navegacion-principal a:contains("Invitados")').addClass('activo');
+
+    // Menu fijo
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if(scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top': barraAltura+'px'});
+        }else {
+            $('.barra').removeClass('fixed');
+            $('body').css({'margin-top': '0px'});
+        }
+
+        //console.log(scroll);
+    });
+
+    // Menu Responsive
+    $('.menu-movil').on('click', function(){
+        $('.navegacion-principal').slideToggle();
+    });
+
+    //Programa de Conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function() {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        
+        $('.ocultar').hide();
+        
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+
+        return false;
+    });
+
+    //Animaciones para los Numeros
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
+    //Cuenta Regresiva
+    $('.cuenta-regresiva').countdown('2020/11/11 10:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
+    // Colorbox
+    $('.invitado-info').colorbox({inline:true, width:"50%"});
+    $('.boton_newsletter').colorbox({inline:true, width:"50%"});
+});
+
+// $(function(){
+//     $('.invitado-info').colorbox({inline:true, width:"50%"});
+    
+//    });
